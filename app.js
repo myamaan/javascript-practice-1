10e8e23bcd2ec3d052761c8094cad83afc59b1a5
myamaan <yamaan.hanif777@gmail.com>
2025-05-12 22:46:12 -0700

    ..

---
+++ app.js
@@ -0,0 +1,12 @@
+let daal = 100; // gram
+let chawal = 200; 
+let total = daal + chawal;
+console.log(`Total khana ${total} gram.`);
+
+
+let paise = 500;
+if (paise >= 300) { 
+ console.log("Biryani khareed saktay hain.");
+} else {
+    console.log("Maggie banao");   
+}
